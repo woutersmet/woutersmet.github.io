58f3d89eb9eeadc71ea5996bd831dbfb7bd08a56
Wouter Smet <wouter@engagor.com>
2015-02-09 12:04:53 +0100

    move angularfire seed project + no more teamview

--- teamview/teamview/auth/authControllers.js
+++
@@ -1,7 +0,0 @@
-/*
-* AUTHENTICATION CONTROLLERS
- */
-
-  TeamViewAppControllers.controller('TVLoginCtrl', function($scope){
-    $scope.message = 'User login';
-  });
--- teamview/teamview/feed/feedControllers.js
+++
@@ -1,9 +0,0 @@
-/*
-* USERS CONTROLLERS
- */
-
-  TeamViewAppControllers.controller('TVFeedIndexCtrl', function($scope){
-    $scope.reports = global.app.reports;
-    $scope.message = 'Feeds';
-    $scope.showfeed = true;
-  });
