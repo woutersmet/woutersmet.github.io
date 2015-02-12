926c2d1d18becac4ef4603953c2b2f63eb0f7679
Wouter Smet <wouter@engagor.com>
2015-02-12 18:53:38 +0100

    ang-news tutorial

--- ang-news/app/scripts/app.js
+++ ang-news/app/scripts/app.js
@@ -8,24 +8,41 @@
  *
  * Main module of the application.
  */
-angular
+var app = angular
   .module('angNewsApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'firebase'
   ])
+
+  .constant ('FIREBASE_URL', 'http://woutertest.firebaseio.com/')
+
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+        templateUrl: 'views/posts.html',
+        controller: 'PostsCtrl'
       })
       .when('/about', {
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
+      })
+      .when('/posts/:postId', {
+        templateUrl : 'views/showpost.html',
+        controller : 'PostViewCtrl'
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'AuthCtrl',
+        resolve: {
+          user: function(Auth) {
+            return Auth.resolveUser();
+          }
+        }
       })
       .otherwise({
         redirectTo: '/'
---
+++ ang-news/app/scripts/controllers/auth.js
@@ -0,0 +1,15 @@
+'use strict';
+
+app.controller('AuthCtrl', function ($scope, $location, Auth, user) {
+  if (user) {
+    $location.path('/');
+  }
+
+  $scope.register = function () {
+    Auth.register($scope.user).then(function() {
+      return Auth.login($scope.user).then(function() {
+        $location.path('/');
+      });
+    });
+  };
+});
