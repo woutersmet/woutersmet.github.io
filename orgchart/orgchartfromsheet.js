204f3e9ad4a7dbe9dd65f66bcc1eab4d0e2574c8
Wouter Smet <wouter.smet@showpad.com>
2019-05-28 14:31:46 +0200

    allow subsubtitle

--- orgchart/orgchartfromsheet.js
+++ orgchart/orgchartfromsheet.js
@@ -18,7 +18,7 @@ function loadChart(sheetid, sheetname) {
   var spreadsheetUrl = 'https://docs.google.com/spreadsheets/d/'+sheetid; //default
 
   //var sheetname = 'orgdata';
-  var range = 'A2:G999';
+  var range = 'A2:H999';
 
   //something like:  'https://docs.google.com/spreadsheets/d/12akgYh-crO4jv7lrsJ5dVrtrXdsxORfLkWdVKNqme_M/gviz/tq?sheet=orgdata&range=A2:D205';
   var src = spreadsheetUrl + '/gviz/tq?sheet=' + sheetname + '&range=' + range;
@@ -34,11 +34,12 @@ function extractRow(dataFromSheet, i){
   var rowInfo = {
   title : dataFromSheet.getValue(i,0),
   subtitle : dataFromSheet.getValue(i,1),
-  avatar : dataFromSheet.getValue(i,2),
-  color : dataFromSheet.getValue(i,3),
-  code : dataFromSheet.getValue(i,4),
-  parentcode : dataFromSheet.getValue(i,5),
-  popupdetails : dataFromSheet.getValue(i,6),
+  subsubtitle : dataFromSheet.getValue(i,2),
+  avatar : dataFromSheet.getValue(i,3),
+  color : dataFromSheet.getValue(i,4),
+  code : dataFromSheet.getValue(i,5),
+  parentcode : dataFromSheet.getValue(i,6),
+  popupdetails : dataFromSheet.getValue(i,7),
   };
 
   var background = rowInfo.color == null ? '#eee' : rowInfo.color;
@@ -101,7 +102,8 @@ function handleSheetResponse(response) {
 
   var imgPart = row.avatar != null ? '<img class="node-avatar" src="'+row.avatar+'" />' : ''
   var subtitlePart = row.subtitle != null  ? '<div class="node-subtitle">'+row.subtitle+'</div>' : '';
-  var formatted = '<div class="node"><div class="node-header">'+imgPart+'<div class="node-title">' +row.title+ '</div><div style="clear:both;"></div></div>'+subtitlePart+'</div>';
+  var subsubtitlePart = row.subsubtitle != null  ? '<div class="node-subsubtitle">'+row.subsubtitle+'</div>' : '';
+  var formatted = '<div class="node"><div class="node-header">'+imgPart+'<div class="node-title">' +row.title+ '</div><div style="clear:both;"></div></div>'+subtitlePart+subsubtitlePart+'</div>';
 
   var newRow = [{v : row.code, f : formatted},row.parentcode,''];
   data.addRow(newRow);
